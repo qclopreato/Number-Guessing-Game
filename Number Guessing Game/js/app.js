65dbea2be532875b544ba77ba05c11cd78a56187
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-03-06 12:26:45 -0600

    Update app.js

--- Number Guessing Game/js/app.js
+++ Number Guessing Game/js/app.js
@@ -4,25 +4,26 @@ let computerScr = 0;
 
 document.getElementById(`submit`).addEventListener(`click`, function(){
     let humanGuess = document.getElementById(`guess`).value;
-   document.getElementById(`humanOut`).innerHTML = `Human Guess: ` + humanGuess;
 
-   let computerGuess = Math.floor(Math.random()* 10);
-   document.getElementById(`computer`).innerHTML = `Computer Guess: ` + computerGuess;
+    document.getElementById(`humanOut`).innerHTML = humanGuess;
 
-   let randomNumber = myArray[Math.floor(Math.random() * myArray.length)];
-   document.getElementById(`outputNumber`).innerHTML = `Random Number: ` + randomNumber;
-   
-   let humanDiff = Math.abs(randomNumber - humanGuess);
-
-   let computerDiff = Math.abs(randomNumber - computerGuess);
-
-   if(humanDiff > computerDiff){
-    computerScr++;
-    document.getElementById(`computerScr`).innerHTML = `Computer Score: ` + computerScr;
-   } else if (humanDiff < computerDiff){
-    humanScr++;
-    document.getElementById(`humanScr`).innerHTML = `Human Score: ` + humanScr;
-   } else if (humanDiff === computerDiff){
-    return;
-   }
+    let computerGuess = Math.floor(Math.random()* 10);
+    document.getElementById(`computer`).innerHTML = computerGuess;
+ 
+    let randomNumber = myArray[Math.floor(Math.random() * myArray.length)];
+    document.getElementById(`outputNumber`).innerHTML = randomNumber;
+    
+    let humanDiff = Math.abs(randomNumber - humanGuess);
+ 
+    let computerDiff = Math.abs(randomNumber - computerGuess);
+ 
+    if(humanDiff > computerDiff){
+     computerScr++;
+     document.getElementById(`computerScr`).innerHTML = computerScr;
+    } else if (humanDiff < computerDiff){
+     humanScr++;
+     document.getElementById(`humanScr`).innerHTML = humanScr;
+    } else if (humanDiff === computerDiff){
+     return;
+    }
 });
